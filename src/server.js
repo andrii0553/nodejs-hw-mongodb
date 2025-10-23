c11a09fe8823a179bd69208dd6a1ecb9434c03aa
andrii0553 <baydak0553@gmail.com>
2025-10-23 19:12:58 +0300

    init

--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+
   app.use(express.json());
   app.use(cors());
 
@@ -36,6 +37,8 @@ export const setupServer = () => {
 
   /* app.use(contactsRouter); */
   app.use(router);
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(notFoundHandler);
   app.use(errorHandler);
   /* app.get('/contacts', async (req, res) => {
@@ -76,6 +79,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 };
